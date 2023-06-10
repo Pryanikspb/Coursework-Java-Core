07e64cb46bc39c354e3e6019703b9d680c112cb9
Pryanikspb <konstantinprivator@gmail.com>
2023-06-10 10:54:31 +0300

    Initial commit

---
+++ src/main/java/pro/sky/courseworkJavaCore/CourseworkJavaCoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.courseworkJavaCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkJavaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseworkJavaCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/courseworkJavaCore/CourseworkJavaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.courseworkJavaCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkJavaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
